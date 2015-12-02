e2381ccfdff4f7f5ce889bdedd672cc1c2486973
dmytrol <dmytrol@wix.com>
2015-12-02 23:16:41 +0200

    git push -u origin master

--- src/main/java/com/lukianchuk/automationTest/Main.java
+++ src/main/java/com/lukianchuk/automationTest/Main.java
@@ -1,7 +1,12 @@
 package com.lukianchuk.automationTest;
 
+
 /**
  * Created by qa1 on 12/2/15.
  */
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("hallo");
+    }
+
 }
