a04c54ce69b8e95ef0b5de2a847f93508a00fa7d
dmytrol <dmytrol@wix.com>
2015-12-02 23:18:21 +0200

    second

--- src/main/java/com/lukianchuk/automationTest/Main.java
+++ src/main/java/com/lukianchuk/automationTest/Main.java
@@ -6,7 +6,7 @@ package com.lukianchuk.automationTest;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hallo");
+        System.out.println("halloasdfasfasdf");
     }
 
 }
